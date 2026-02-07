f1e921ffdf868e3b5488922655c08ae6e7a7b266
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-02-08 00:15:57 +0530

    added project files

---
+++ Firstdemo.py
@@ -0,0 +1,14 @@
+print ("hello")
+
+# for comments in python we use # for single line comments and ''' for multi line comments
+a= 3
+
+print(a)
+
+str = "Hello world"
+
+print (str)
+
+b,c,d = 5, 6.4 , "Fox"
+print(b,c,d)
+
