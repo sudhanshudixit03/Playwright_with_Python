3a26c0c7d8c1dcf9d5c87e10330b712e9e0d0669
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-03-17 11:15:39 +0530

    chagnes

---
+++ While_loop.py
@@ -0,0 +1,14 @@
+it = 4
+while it > 1:
+    print (it)
+    it = it -1
+print ("while loop is completed")
+
+# now only want to 4 and 2 as output
+
+it = 4
+while it > 1:
+    if it != 3:
+        print (it)
+    it = it -1
+print ("while loop is completed")
