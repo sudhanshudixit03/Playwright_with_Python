9ab02cf5b4861356f2efb8be89086f8695e1fb4c
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-03-20 19:13:46 +0530

    new

--- function demo.py
+++ function demo.py
@@ -1,7 +1,24 @@
 # for creating a function we use the keyword "def" followed by the name of the function and parentheses.
+
+
 def GreetMe():
     print ("Good morning")
 
 # for callimg the function we use the name of the function followed by parentheses.
 
 GreetMe()
+
+
+
+# parameterized function = set parameter into function
+
+def GreetMe(name):
+    print("Good morning"+name)
+
+GreetMe("Sid")
+
+# add integers into function
+
+def AddIntegers(a,b):
+    print(a+b)
+AddIntegers(1,2)
