6ba48db078b7930076b39a01c6bf5c58de4e3079
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-03-20 18:08:14 +0530

    new changes

---
+++ function demo.py
@@ -0,0 +1,7 @@
+# for creating a function we use the keyword "def" followed by the name of the function and parentheses.
+def GreetMe():
+    print ("Good morning")
+
+# for callimg the function we use the name of the function followed by parentheses.
+
+GreetMe()
