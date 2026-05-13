f12fa46cdfba650e8fa569e5b488df0bd4e1129d
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-05-13 12:06:39 +0530

    changes

--- OOPS_demo.py
+++ OOPS_demo.py
@@ -2,9 +2,8 @@ class calculator:
     num=100
 
     def getdata(self):
-        print("I am always right")
+        print("I am now executing as method in class")
 
 obj = calculator()
 obj.getdata()
 print(obj.num)
-
